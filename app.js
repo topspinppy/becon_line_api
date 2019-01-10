3c2b39e39fd340e6524984c634503ed547ba8f13
Paranat K <5806021623049@fitm.kmutnb.ac.th>
2019-01-10 18:01:29 +0700

    üpdata pack.json

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const axios = require('axios')
 
 const app = express()
 const port = process.env.PORT || 4000
-const hostname = 'line-bot-tesa-nernhom400.herokuapp.com'
 const HEADERS = {
 	'Content-Type': 'application/json',
 	'Authorization': 'Bearer HUWzoOZ9yNeWTjO3I9yWCC7NeY0nKnjV9kh8fVhCqhwpKQ//1vYUIbTAnuY2pSHrtBI45IfcgD2j8ft2/8N5Rzl7frDbcafwBLPEVK+aJOVY6bBUS+MlRlggZA4RD3eS4n/5WnEc+0qNQYdgCSLesgdB04t89/1O/w1cDnyilFU='
@@ -73,6 +72,6 @@ function curl(method, body) {
 	})
 }
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
 	console.log(`Server running`)
 })
