5b89e2ed73ef60772898d60828e5f1b8721e8835
Paranat K <5806021623049@fitm.kmutnb.ac.th>
2019-01-10 17:43:33 +0700

    line bot

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const axios = require('axios')
 
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.PORT || 4000
 const hostname = 'line-bot-tesa-nernhom400.herokuapp.com'
 const HEADERS = {
 	'Content-Type': 'application/json',
@@ -74,5 +74,5 @@ function curl(method, body) {
 }
 
 app.listen(port, hostname, () => {
-	console.log(`Server running at https://${hostname}:${port}/`)
+	console.log(`Server running`)
 })
